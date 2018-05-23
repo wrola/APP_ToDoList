e61b58aa6a57382a2578cd8d80c6342e1491a9a4
wrola <rola.wojciech@gmail.com>
2018-05-23 10:47:13 +0200

    update structure of project

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+var path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+
+
+module.exports = {
+    devtool: 'cheap-eval-source-map',
+    entry: {
+        main: path.join(__dirname, 'src', 'app.js')
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 9000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015', 'stage-2', 'react']
+                    }
+                }
+            },
+            {
+                test: /\.(png|jpe?g|svg|gif|woff|otf)$/,
+                use: {loader: 'file-loader'}
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ['css-loader', 'sass-loader']
+                })
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'index.html'),
+            hash: true
+        }),
+        new ExtractTextPlugin({
+            filename: 'app.bundle.css'
+        })
+    ]
+}
